73d19f1701cc3c27232fdf0d0ac40dbe1466ee3d
Octavio Herrera Contreras <156601957+Octavi00@users.noreply.github.com>
2026-04-16 21:06:34 -0700

    This is my first commit. I've setup the very basis of the website starting with the homepage, a couple useful buttons, and the idea of loaders/rendering functions. Stil just a start

--- scripts.js
+++ scripts.js
@@ -30,68 +30,124 @@ const CURB_POSTER_URL =
 const EAST_LOS_HIGH_POSTER_URL =
   "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
+
+//basic url's
+/*
+*/
+
+
+
 // This is an array of strings (TV show titles)
 let titles = [
   "Fresh Prince of Bel Air",
   "Curb Your Enthusiasm",
   "East Los High",
 ];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
 
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
+let main_part_categories = [
+  "Front Forks",
+  "Brake systems",
+  "Rear Suspension",
+  "Tires",
+  "Powertrains",
+  "Misceleaneous",
+  "All",
+]
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+//Here are some more specific category breakdowns for generalized names
+let brake_systems_parts = [
+  "Fluids",
+  "Pads",
+  "Calipers",
+  "Hoses",
+  "Levers",
+  "Brake Kits",
+]
 
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+let powertrains_parts = [
+  "Chains",
+  "Deraileurs",
+  "Cassettes",
+  "Cranksets",
+]
+/////////////////////////////////////////////////////
+
+const loaders = {
+  homepage : loadHomePage,
+  aboutme : loadAboutPage,
+}
+
+//Page loaders
+function loadHomePage()
+{
+  const mainCategory_Container = document.getElementById("main-category-container");
+  mainCategory_Container.innerHTML = ""; //homepage url extension
+  const templateCategory = document.querySelector(".category");
+  
+  for (let i = 0; i < main_part_categories.length; i++)
+  {
+    let title = main_part_categories[i];
+    
+    //NOTE: make a function to get and insert urls
+    let imageURL = ""
+    
+
+    const nextCategory = templateCategory.cloneNode(true); //Copy this created template
+    
+    editCategoryContent(nextCategory, title, imageURL); // Add a title + image
+    mainCategory_Container.appendChild(nextCategory); // Add into category container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
-
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
-
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
+function loadAboutPage()
+{
   alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
+    "This is a good stopping point"
   );
 }
 
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
+
+
+function editCategoryContent(category, newTitle, newImageURL){
+  category.style.display = "block";
+
+  const categoryHeader = category.querySelector("h2");
+  const categoryImage = category.querySelector("img");
+  const link = category.querySelector(".category-link");
+
+  categoryHeader.textContent = newTitle;
+
+  categoryImage.src = newImageURL;
+  categoryImage.alt = newTitle + " Image";
+  
+  //loads new page when Title or image is clicked
+  link.href = "index.html?category=" + encodeURIComponent(newTitle);
+
+  //prints for debugging
+  console.log("New category: ", newTitle, "- html: ", category);
+}
+
+
+
+
+//Default Page loader when website is openned
+document.addEventListener("DOMContentLoaded", loaders["homepage"]);
+
+
+
+
+
+
+
+
+//button functions
+function home_page_clicked() {
+  console.log("Home Page clicked");
+  loaders["homepage"]?.();
+}
+
+function about_page_clicked() {
+  console.log("About Page clicked");
+  loaders["aboutme"]?.();
 }

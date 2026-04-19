71e2dfe0a1d6fa754c415de0e9be55c68aab8c11
Octavio Herrera Contreras <156601957+Octavi00@users.noreply.github.com>
2026-04-18 21:20:30 -0700

    A lot was done in this continuous sitting. Big changes, css: standardized theme for background image, data: added jsons to ALL categories, category.js: modularized page rendering + loading to allow for All category to operate separately from other categories. Also used arrays to plan for future filters. What I want to do next: I want to add support for filters, as well as add an admin/creation page that lets a user insert a new item by utilizing a password and modifying apropriate json files. Good night

--- script_controllers/categories.js
+++ script_controllers/categories.js
@@ -72,42 +72,119 @@ function renderItems(items)
 
 //Note: uses async to wait on loading page while function finishes
 async function loadPage() {
+  const params = new URLSearchParams(window.location.search);
+  const category = params.get("data");
 
-    //I will need to implement another way of fetching all data later
-    const params = new URLSearchParams(window.location.search);
-    const dataFile = params.get("data");   
+  if (category === "all") {
+    await allCategoriesPage();
+  }
+  else {
+    await singleCategoryPage(category);
+  }
+}
+  
+  
 
-    //use a try and catch block to handle potential errors in fetching data, such as incorrect file paths or missing files, so user is aware of the issue instead of just a blank page
-    try {
-      const response = await fetch(`../data_files/${dataFile}.json`);
-      if (!response.ok) {
-        console.error("Failed to load data: ", response.statusText);
-        alert("No data available for the requested category.");
-        return;
-      }
+
+async function singleCategoryPage(category) {
+  //fetch data
+  const data = await fetchData(category);
+
+  if (!data) {
+    // If data is null, return early, error is already handled in fetchData()
+    return;
+  }
+
+  //set theme image
+  setBackgroundImage(data.background_image);
+  
+  //Set title and description
+  document.getElementById("title").textContent = data.title;
+  document.getElementById("description").textContent = data.description;
+  
+  //build items array and keywords array for later use in filters
+  all_items = buildItemsArray(data.items);
+  all_keywords = [...new Set(all_items.flatMap(item => item.keywords))];
     
-      //If data fails to load here, an error will be thrown and caught in the catch block
-      const data = await response.json();
+  //now render all items as default page view
+  renderItems(all_items);     
+}
 
 
-      //set Theme
-      document.body.classList.add(data.theme);
+async function allCategoriesPage() {
+  //run a for loop fetching all data files and building items array and keywords array for each, then render all items together on page
+  let all_items_combined = [];
+  let all_keywords_combined = new Set();
 
-      //Set title and description
-      document.getElementById("title").textContent = data.title;
-      document.getElementById("description").textContent = data.description;
+  //Note: data here implies data from all.json wihtin categories folder
+  const data = await fetchData("all");
+  if (!data) {
+    // If null, return early, error is already handled in fetchData()
+    return;
+  }
 
-      //build items array and keywords array for later use in filters
-      all_items = buildItemsArray(data.items);
-      all_keywords = [...new Set(all_items.flatMap(item => item.keywords))];
-    
-      //now render all items as default page view
-      renderItems(all_items);
+  //set theme image
+  setBackgroundImage(data.background_image);
+  
+  //Set title and description
+  document.getElementById("title").textContent = data.title;
+  document.getElementById("description").textContent = data.description;
+  
+  const categories = data.categories;
 
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      alert("An error occurred while fetching data. Please try again later.");
+  //now loop through each category
+  for (const category of categories) {
+    const data = await fetchData(category);
+    if (data && data.items) {
+      const items = buildItemsArray(data.items);
+      all_items_combined = all_items_combined.concat(items);
+      items.forEach(item => item.keywords.forEach(keyword => all_keywords_combined.add(keyword)));
     }
+  }
+
+  //now rewrite global arrays with combined data and render all items together on page
+  all_items = all_items_combined;
+  all_keywords = Array.from(all_keywords_combined);
+  renderItems(all_items);
+}
+
+
+
+
+async function fetchData(category) {
+  const dataFile = "categories/" + category;
+  
+  //use a try and catch block to handle potential errors in fetching data, such as incorrect file paths or missing files, so user is aware of the issue instead of just a blank page
+  try {
+    const response = await fetch(`../data_files/${dataFile}.json`);
+    if (!response.ok) {
+      console.error("Failed to load data: ", response.statusText);
+      alert("No data available for the requested category.");
+      return;
+    }
+    
+    //If data fails to load here, an error will be thrown and caught in the catch block
+    const data = await response.json();
+    return data;
+  } 
+  catch (error) {
+    console.error("Error fetching data:", error);
+    alert("An error occurred while fetching data. Please try again later.");
+  }
+
+  return null; // Return null if there was an error
+}
+
+
+//small helper function for setting background image
+async function setBackgroundImage(image_url) {
+  if (image_url && image_url !== "") {
+    document.body.style.setProperty('background-image', `url("${image_url}")`);
+  }
+  else {
+    // Set none background image if none provided in data
+    document.body.style.setProperty('background-image', 'none');
+  }
 }
 
 
